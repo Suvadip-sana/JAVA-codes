916d2763062be257e5f304fad180615f94120ec0
Suvadip-sana <78638404+Suvadip-sana@users.noreply.github.com>
2024-02-05 14:34:13 +0530

    Add files via upload

---
+++ Java Codes/j_02_Literals.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class j_02_Literals {
+    public static void main(String[] args) {
+        byte age = 58;
+        int age2 = 30;
+        short age3 = 89;
+        long money = 43789000l;
+        char c = 'A';
+        float f1 = 8.9f;
+        double d1 = 6.8754;
+        boolean b = true;
+        System.out.println("Age = "+age+"\nMoney = "+money+"\nThe character is = "+c+"\nDecimal number is = "+d1+"\nThe statement is: "+b);
+
+    }
+}
