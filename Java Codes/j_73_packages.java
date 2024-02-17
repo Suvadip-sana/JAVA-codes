e57a4cb7d138078c41fdb3f9a9acab64a1bd2ae8
Suvadip Sana <78638404+Suvadip-sana@users.noreply.github.com>
2024-02-18 00:26:03 +0530

    Add files via upload

---
+++ Java Codes/j_73_packages.java
@@ -0,0 +1,13 @@
+package com.company;
+//import java.util.Scanner;
+//import java.util.*;
+
+public class j_73_packages {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+        System.out.println("This is my scanner taking input as integer that is: " + a);
+    }
+}
