e75bffe83e7fafc1383ba95da0ba4b28a12860f2
Suvadip Sana <78638404+Suvadip-sana@users.noreply.github.com>
2024-02-18 00:27:50 +0530

    Add files via upload

---
+++ Java Codes/j_75_Creating_custom_package.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/*
+You have to create a package named com.codes.shape
+This package should have individual classes for Rectangle, Square, Circle, Cylinder, Sphere
+These classes should use inheritance to properly manage the code!
+Include methods like volume, surface area and getters/setters for dimensions
+ */
+public class j_75_Creating_custom_package {
+    public static void main(String[] args) {
+
+    }
+}
