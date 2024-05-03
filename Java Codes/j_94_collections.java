bef76c79290795e9a5d50f9ffe65aa20325d1b7a
Suvadip Sana <suvadipsana602@gmail.com>
2024-05-03 23:55:45 +0530

    Add files via upload

---
+++ Java Codes/j_94_collections.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class j_94_collections {
+    public static void main(String[] args) {
+//        ArrayList;
+//        Set;
+//        TreeSet;
+    }
+}
