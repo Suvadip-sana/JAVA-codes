3bc07b457ff5bfd6968669facf4540a229005998
Suvadip Sana <suvadipsana602@gmail.com>
2024-07-06 14:18:08 +0530

    Add files via upload

---
+++ Java Codes 2/j_103_time.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class j_103_time {
+    public static void main(String[] args) {
+
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("EEE, d mm YYYY, [HH:mm:ss)");
+
+    }
+}
