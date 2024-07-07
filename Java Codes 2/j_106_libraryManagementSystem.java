f66f20dde9971b9a48b7cf5ff47ed6ad303e3ba3
Suvadip Sana <suvadipsana602@gmail.com>
2024-07-08 01:41:38 +0530

    Add files via upload

---
+++ Java Codes 2/j_106_libraryManagementSystem.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class j_106_libraryManagementSystem {
+    public static void main(String[] args) {
+        /*
+        Create a Library Management system which is capable of issuing books to the students.
+        Books should have information like:
+        1. Book Name.
+        2. Book Author.
+        3. Issued to.
+        4. Issued on.
+        User should be able to add books, return issued books, issue books.
+        Assume that all the user are registered with their names in the central database
+         */
+    }
+}
